8bebe8efae55df6377445d3dc61f191316490b89
Abadie Celian <celian.abadie@etu.iut-tlse3.fr>
2023-03-31 09:08:37 +0200

    testGetter rouge qui passe pas

--- src/application/TestCouleur.java
+++ src/application/TestCouleur.java
@@ -2,14 +2,20 @@ package application;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 
 class TestCouleur {
 
 	@Test
-	public void whenExceptionThrown_thenRuleIsApplied() {
+	public void testConstructeurCouleur() {
 		assertThrows(IllegalArgumentException.class, () -> new Couleur("Une couleur",0,0,300));
 	}
+	
+	@Test
+	public void testGetterRouge() {
+		Couleur rouge = new Couleur("Une couleur",200,0,0);
+		assertEquals(200,rouge.getRouge());
+	}
+
 
 }
