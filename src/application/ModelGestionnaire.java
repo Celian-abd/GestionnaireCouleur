96f009d18b8945f32adb701b72cff3768ddecfa8
Abadie Celian <celian.abadie@etu.iut-tlse3.fr>
2023-03-31 09:33:44 +0200

    constructeur du ModelGestionnaire

--- src/application/ModelGestionnaire.java
+++ src/application/ModelGestionnaire.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.paint.Color;
+
 public class ModelGestionnaire {
-	
+
 	private List<Couleur> couleurs;
-	private int indiceParcour;
+    private String nomFichier;
+    private int index;
+    private SimpleObjectProperty<Color> currentColor;
+    private SimpleStringProperty currentIndexLabel;
+    
+    public ModelGestionnaire() {
+        this.couleurs = new ArrayList<>();
+        this.nomFichier = "";
+        this.index = 0;
+        this.currentColor = new SimpleObjectProperty<>();
+        this.currentIndexLabel = new SimpleStringProperty();
+    }
 
 }
